8571570e7f0e649cea011e66f5108eb2d225b3c6
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-31 14:54:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-
+    "My name is Anne-Laure"
   end
 
   get '/hometown' do
-
+    "My honetown is Saint Mande"
   end
 
   get '/favorite-song' do
-
+    "My favorite song is I'm still standing"
   end
 
 
