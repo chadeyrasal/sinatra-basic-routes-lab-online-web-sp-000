daff95ee5dac0027c1ac053b24f07ad8fb09434e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-31 14:52:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,16 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    
+
   end
 
   get '/hometown' do
-    
+
   end
 
   get '/favorite-song' do
-    
+
   end
 
-  
+
 end
